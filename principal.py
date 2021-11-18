8f2b510302e0a16e780dfdd8efed5b4b28040346
Nadia Testani <nadiatestani@gmail.com>
2021-11-18 14:48:30 -0300

    se agrega parte de nadia

--- principal.py
+++ principal.py
@@ -1,9 +1,11 @@
+import pandas as pd
+
 from tp_python_unsam.support import estaciones_inta
-from tp_python_unsam.support import archivos
+from tp_python_unsam.support import archivos 
 from tp_python_unsam.procesamiento import procesar_datos_pandas
 
-inicio_periodo = '1983-01-01'
-fin_periodo = '2017-12-01'
+inicio_periodo = '1983-07-01'
+fin_periodo = '2017-06-01'
 
 
 def obtener_datos_estacion_inta(estacion_id):
@@ -21,3 +23,21 @@ ubicacion = estaciones_inta.EstacionInta(estacion_id).ubicacion()
 # list=obtener_datos_estacion_inta(estacion_id)
 # print(obtener_datos_estacion_inta(estacion_id))
 print(obtener_datos_estacion_inta(estacion_id))
+
+###
+#obtener lon y lat cercanas a punto inta
+###
+
+longitud = 300.5 #ejemplo
+latitud = -27.5 #ejemplo
+
+def abre_y_acomoda_nc(inicio_periodo, fin_periodo, longitud, latitud):
+    """
+    Abre archivos nc y los acomoda en un array3D?
+    """
+    fechas = pd.date_range(start=inicio_periodo, end= fin_periodo, freq="M")
+    serie = [archivos.leer_archivo_nc(fecha, longitud, latitud)["cldamt"].values[0] for fecha in fechas]
+    panda_serie = pd.DataFrame(serie, index = fechas, columns = ["ISCCP"])
+    return panda_serie
+
+print(abre_y_acomoda_nc(inicio_periodo, fin_periodo, longitud, latitud))
--- support/archivos.py
+++ support/archivos.py
@@ -22,3 +22,40 @@ def leer_archivo(id_estacion):
         open(ruta_completa, 'wb').write(r.content)
         print('Abriendo archivo de datos...','\n')
     return pd.read_excel(ruta_completa)
+
+
+# leer archivos nc
+import xarray as xr
+
+directorio_datos_nc = f'{os.path.dirname(os.getcwd())}/python_unsam/tp_python_unsam/archivos_nc'
+
+def leer_archivo_nc(fecha, longitud, latitud):
+    """
+    Fecha = str 'yyyy-mm-dd'
+    Verifica si el directorio de datos (../archivos_nc) existe. Si no existe, lo crea.
+    Luego, verifica si el archivo de datos correspondiente a esas fechas existe en el directorio de datos.
+    Si no existe, lo descarga desde el repositorio de la NOAA, lo guarda en el directorio de datos.
+    Abre el dato del nc de fecha para el punto (longitud, latitud)"""
+    
+    #fecha_dt = datetime.datetime.strptime(fecha, "%Y-%m-%d")
+    anio = fecha.year
+    mes = fecha.month
+    
+    if len(str(mes))== 1: #si es un mes de un digito, agrego 0 antes del mes
+        nombre_archivo = f"ISCCP-Basic.HGM.v01r00.GLOBAL.{anio}.0{mes}.99.9999.GPC.10KM.CS00.EA1.00.nc"
+    else: #sino no
+        nombre_archivo = f"ISCCP-Basic.HGM.v01r00.GLOBAL.{anio}.{mes}.99.9999.GPC.10KM.CS00.EA1.00.nc"
+    
+    ruta_completa = os.path.join(directorio_datos_nc, nombre_archivo)
+    
+    if not os.path.exists(directorio_datos_nc):
+        os.makedirs(directorio_datos_nc)
+    
+    if not os.path.isfile(ruta_completa): #descargo el dato 
+        url = f"https://www.ncei.noaa.gov/data/international-satellite-cloud-climate-project-isccp-h-series-data/access/isccp-basic/hgm/{nombre_archivo}"
+        print('Descargando archivo de datos...','\n')
+        r = requests.get(url, allow_redirects=True)
+        open(ruta_completa, 'wb').write(r.content)        
+    
+    data = xr.open_dataset(ruta_completa)[["cldamt"]].loc[dict(lon = longitud,lat = latitud)] #abro el archivo, selecciono solo la variable "cldamt" y selecciono el punto de lon y lat a usar 
+    return(data)
