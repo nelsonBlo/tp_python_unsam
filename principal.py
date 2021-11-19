e8919eccdb2e81395c55c17b0a1c52147d76d017
Nelson Bocanegra Lopez <nelson.bocanegra@rappi.com>
2021-11-19 16:21:36 -0300

    finished

--- principal.py
+++ principal.py
@@ -1,11 +1,14 @@
+import datetime
+
+import numpy as np
 import pandas as pd
 
-from tp_python_unsam.support import estaciones_inta
-from tp_python_unsam.support import archivos 
-from tp_python_unsam.procesamiento import procesar_datos_pandas
+from support import estaciones_inta
+from support import archivos
+from procesamiento import procesar_datos_pandas
 
 inicio_periodo = '1983-07-01'
-fin_periodo = '2017-06-01'
+fin_periodo = '1983-10-01'
 
 
 def obtener_datos_estacion_inta(estacion_id):
@@ -13,7 +16,7 @@ def obtener_datos_estacion_inta(estacion_id):
         print('No se encontró la estación.')
         return None
     else:
-        datos = archivos.leer_archivo(estacion_id)
+        datos = archivos.leer_archivo_xls(estacion_id)
         subset = procesar_datos_pandas.obtener_promedios_mensual_inta(datos, inicio_periodo, fin_periodo)
         return subset, ubicacion
 
@@ -24,20 +27,37 @@ ubicacion = estaciones_inta.EstacionInta(estacion_id).ubicacion()
 # print(obtener_datos_estacion_inta(estacion_id))
 print(obtener_datos_estacion_inta(estacion_id))
 
-###
-#obtener lon y lat cercanas a punto inta
-###
 
-longitud = 300.5 #ejemplo
-latitud = -27.5 #ejemplo
+###
+# obtener lon y lat cercanas a punto inta
+def punto_cercano(ubicacion, latitud_satelite, longitud_satelite):
+    longitud_satelite = np.array(longitud_satelite)
+    latitud_satelite = np.array(latitud_satelite)
+
+    diferencia_longitudes = abs(longitud_satelite - (ubicacion[1] + 360))
+    diferencia_latitudes = abs(latitud_satelite - ubicacion[0])
+    long = longitud_satelite[diferencia_longitudes == diferencia_longitudes.min()]
+    lat = latitud_satelite[diferencia_latitudes == diferencia_latitudes.min()]
+    print(f'el punto más cercano a la estación es: {long-360, lat}')
+    return long, lat
+
+###
+longitud_satelite = archivos.leer_grilla_nc(datetime.datetime(1984, 1, 1))[0]  # ejemplo
+latitud_satelite = archivos.leer_grilla_nc(datetime.datetime(1984, 1, 1))[1]  # ejemplo
+
+ubicacion_satelite = punto_cercano(ubicacion, latitud_satelite, longitud_satelite)
+
 
 def abre_y_acomoda_nc(inicio_periodo, fin_periodo, longitud, latitud):
     """
     Abre archivos nc y los acomoda en un array3D?
     """
-    fechas = pd.date_range(start=inicio_periodo, end= fin_periodo, freq="M")
-    serie = [archivos.leer_archivo_nc(fecha, longitud, latitud)["cldamt"].values[0] for fecha in fechas]
-    panda_serie = pd.DataFrame(serie, index = fechas, columns = ["ISCCP"])
+    fechas = pd.date_range(start=inicio_periodo, end=fin_periodo, freq="M")
+    serie = [archivos.leer_nubosidad_nc(fecha, longitud, latitud)[0] for fecha in fechas]
+    panda_serie = pd.DataFrame(serie, index=fechas, columns=["ISCCP"])
     return panda_serie
 
-print(abre_y_acomoda_nc(inicio_periodo, fin_periodo, longitud, latitud))
+
+# print(punto_cercano(ubicacion, longitud, latitud, ))
+
+print(abre_y_acomoda_nc(inicio_periodo, fin_periodo, ubicacion_satelite[0], ubicacion_satelite[1]))
--- support/archivos.py
+++ support/archivos.py
@@ -1,61 +1,64 @@
 import os
 import pandas as pd
 import requests
+import xarray as xr
 
-directorio_datos = f'{os.path.dirname(os.getcwd())}/archivos'
+directorio_datos_inta = f'{os.path.dirname(os.getcwd())}/tp_python_unsam/archivos_inta'
+directorio_datos_nc = f'{os.path.dirname(os.getcwd())}/tp_python_unsam/archivos_nc'
 
 
-def leer_archivo(id_estacion):
+def leer_archivo_xls(id_estacion):
     """Verifica si el directorio de datos (../archivos) existe. Si no existe, lo crea.
     Luego, verifica si el archivo de datos correspondiente a la estación existe en el directorio de datos.
     Si no existe, lo descarga desde el repositorio del INTA, lo guarda en el directorio de datos y lo entrega
     como dataframe de pandas.
     Si ya existe, lo abre y lo entrega como dataframe de pandas.
     """
-    ruta_completa = f'{directorio_datos}/{id_estacion}.xls'
-    if not os.path.exists(directorio_datos):
-        os.makedirs(directorio_datos)
+    ruta_completa = os.path.join(directorio_datos_inta, f'{id_estacion}.xls')
+    if not os.path.exists(directorio_datos_inta):
+        os.makedirs(directorio_datos_inta)
     if not os.path.isfile(ruta_completa):
         url = f'http://siga2.inta.gov.ar/document/series/{id_estacion}.xls'
-        print('Descargando archivo de datos...','\n')
+        print('Descargando archivo de datos...', '\n')
         r = requests.get(url, allow_redirects=True)
         open(ruta_completa, 'wb').write(r.content)
-        print('Abriendo archivo de datos...','\n')
+        print('Abriendo archivo de datos...', '\n')
     return pd.read_excel(ruta_completa)
 
 
-# leer archivos nc
-import xarray as xr
-
-directorio_datos_nc = f'{os.path.dirname(os.getcwd())}/python_unsam/tp_python_unsam/archivos_nc'
-
-def leer_archivo_nc(fecha, longitud, latitud):
+def leer_nc(fecha):
     """
-    Fecha = str 'yyyy-mm-dd'
-    Verifica si el directorio de datos (../archivos_nc) existe. Si no existe, lo crea.
-    Luego, verifica si el archivo de datos correspondiente a esas fechas existe en el directorio de datos.
-    Si no existe, lo descarga desde el repositorio de la NOAA, lo guarda en el directorio de datos.
-    Abre el dato del nc de fecha para el punto (longitud, latitud)"""
-    
-    #fecha_dt = datetime.datetime.strptime(fecha, "%Y-%m-%d")
+        Fecha = datetime 'yyyy-mm-dd'
+        Verifica si el directorio de datos (../archivos_nc) existe. Si no existe, lo crea.
+        Luego, verifica si el archivo de datos correspondiente a esas fechas existe en el directorio de datos.
+        Si no existe, lo descarga desde el repositorio de la NOAA, lo guarda en el directorio de datos.
+        Abre el dato de cldamt de fecha para el punto (longitud, latitud)"""
+
     anio = fecha.year
-    mes = fecha.month
-    
-    if len(str(mes))== 1: #si es un mes de un digito, agrego 0 antes del mes
-        nombre_archivo = f"ISCCP-Basic.HGM.v01r00.GLOBAL.{anio}.0{mes}.99.9999.GPC.10KM.CS00.EA1.00.nc"
-    else: #sino no
-        nombre_archivo = f"ISCCP-Basic.HGM.v01r00.GLOBAL.{anio}.{mes}.99.9999.GPC.10KM.CS00.EA1.00.nc"
-    
+    mes = f'{fecha.month:02d}'
+    nombre_archivo = f"ISCCP-Basic.HGM.v01r00.GLOBAL.{anio}.{mes}.99.9999.GPC.10KM.CS00.EA1.00.nc"
+
     ruta_completa = os.path.join(directorio_datos_nc, nombre_archivo)
-    
+
     if not os.path.exists(directorio_datos_nc):
         os.makedirs(directorio_datos_nc)
-    
-    if not os.path.isfile(ruta_completa): #descargo el dato 
+
+    if not os.path.isfile(ruta_completa):  # descargo el dato
         url = f"https://www.ncei.noaa.gov/data/international-satellite-cloud-climate-project-isccp-h-series-data/access/isccp-basic/hgm/{nombre_archivo}"
-        print('Descargando archivo de datos...','\n')
+        print('Descargando archivo de datos...', '\n')
         r = requests.get(url, allow_redirects=True)
-        open(ruta_completa, 'wb').write(r.content)        
-    
-    data = xr.open_dataset(ruta_completa)[["cldamt"]].loc[dict(lon = longitud,lat = latitud)] #abro el archivo, selecciono solo la variable "cldamt" y selecciono el punto de lon y lat a usar 
-    return(data)
+        open(ruta_completa, 'wb').write(r.content)
+    return xr.open_dataset(ruta_completa)[["cldamt"]]
+
+
+def leer_grilla_nc(fecha):
+    data = leer_nc(fecha)
+    return data.lon.values, data.lat.values
+
+
+def leer_nubosidad_nc(fecha, longitud, latitud):
+    data = leer_nc(fecha)
+    return data.loc[dict(lon=longitud, lat=latitud)]["cldamt"].values[0]
+
+
+# print(leer_nubosidad_nc(datetime.datetime(1983, 7, 1), 300.5, -40.5))
